8a469991fc40efa3de181a570af0c0bf1e6c79df
homemix <kenmutati@gmail.com>
2022-04-11 17:57:44 +0300

    1. Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import requests
+import csv
+import sys
+
+user_id = sys.argv[1]
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}' \
+        .format(user_id)
+    r = requests.get(url)
+    r_json = r.json()
+    url = 'https://jsonplaceholder.typicode.com/users/{}' \
+        .format(user_id)
+    r = requests.get(url)
+    r_json_user = r.json()
+    user_name = r_json_user.get('username')
+
+    with open('{}.csv'.format(user_id), 'w') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for task in r_json:
+            writer.writerow({
+                'USER_ID': user_id,
+                'USERNAME': user_name,
+                'TASK_COMPLETED_STATUS': task.get('completed'),
+                'TASK_TITLE': task.get('title'),
+            })
+    csvfile.close()
+    print("File {}.csv created successfully".format(user_id))
